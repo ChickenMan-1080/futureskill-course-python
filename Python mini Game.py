2ef355687139a10235ef916dcecabbd4438ff9d4
ChickenMan-1080 <justletmein.comonman@gmail.com>
2026-01-10 07:40:49 +0700

    Add files via upload
    This is Edited version of  project_futureskill

---
+++ Python mini Game.py
@@ -0,0 +1,45 @@
+import sys
+from tkinter import Tk,simpledialog,messagebox
+
+def rec(width,length):
+    loresult = width * length
+    return loresult
+
+root = Tk()
+root.withdraw()
+while True :
+    openBox = ''
+    openBox = simpledialog.askstring('Rectangle calculate','Are you ready? y/n :')
+    if openBox == 'y':
+        while True:
+            input_width = ''
+            input_length = ''
+            input_width = simpledialog.askinteger('Width Input','Enter width : ')
+            input_length = simpledialog.askinteger('Length Input','Ener length : ')
+            if input_width == 6 and input_length == 7:
+                messagebox.showinfo(' 6 7 ?', ' 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! 67! ...')
+                root.destroy()
+                sys.exit()
+            else:
+                result = rec(input_width,input_length)
+                messagebox.showinfo('Answer','The area of Rectangle is ' + str(result))
+                choice = ''
+                choice = simpledialog.askstring('Having fun?','Do you wanna keep going? y/n : ')
+                while choice != 'y' and choice != 'n':
+                    messagebox.showinfo('Excuse me?','what is that even mean? try again')
+                    choice = simpledialog.askstring('Having fun?','So do you wanna keep going? y/n : ')
+
+                if choice == 'n':
+                    messagebox.showinfo('Bye!','Thank you for today , see you next time')
+                    root.destroy()
+                    sys.exit()
+                
+    elif openBox == 'n':
+        messagebox.showinfo('=_=','Bye')
+        root.destroy()
+        sys.exit()
+
+    else :
+        messagebox.showinfo('Huh!?','There are 2 options , follow what it said')
+        
+    
